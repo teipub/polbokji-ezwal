53403befc061bac2fdc542e16b7e227857c28e0f
tei <tei@ai-dapps.com>
2023-12-21 12:28:55 +0900

    예약화면까지 감.

--- index.js
+++ index.js
@@ -14,8 +14,10 @@ const makeBrowser = async () => {
     })
 }
 
+let browser
+
 (async () => {
-    const browser = await makeBrowser()
+     browser = await makeBrowser()
 
     // 로그인
     await browser.url(site.url)
@@ -23,17 +25,21 @@ const makeBrowser = async () => {
     await browser.$('#loginSearchBean_userId').setValue(config.id)
     await browser.$('#loginSearchBean_password1').setValue(config.password)
     await browser.$('.lgn_ip_lgn').click()
-    await waitForTitle(browser, 'cuser/main.ez')
+    await waitForTitle('cuser/main.ez')
 
-    await goToBookablePage(browser)
+    await browser.url('https://polbokji.ezwel.com/cuser/checkin/info/checkinInfoPolForm.ez')
+    await waitForTitle('checkinInfoPolForm.ez')
+    await goToBookablePage()
+    // await browser.url(site.reservation)
+    // await waitForTitle('checkinTrainingCenterBookable.ez')
 
     while(true) {
-        await search(browser)
+        await search()
         await browser.pause(config.waitMilliseconds)
     }
 })().catch((e) => console.error(e))
 
-const waitForTitle = async (browser, title) => {
+const waitForTitle = async (title) => {
     await browser.waitUntil(async function () {
         return -1 < (await browser.getUrl()).indexOf(title)
     }, {
@@ -44,15 +50,17 @@ const waitForTitle = async (browser, title) => {
 
 
 // 예약 페이지로 이동
-const goToBookablePage = async (browser) => {
-    await browser.url(site.reservation)
-    await waitForTitle(browser, 'checkinTrainingCenterBookable.ez')
+const goToBookablePage = async () => {
+    const menu = await browser.$$('ul.lnb_menu>li>a')
+    const url = await menu[2].getAttribute('onclick')
+    await browser.execute(url)
+    await waitForTitle('checkinTrainingCenterBookable.ez')
 }
 
 
-const search = async (browser) => {
+const search = async () => {
     await browser.execute(`goCal('${config.date.substring(0, 6)}01');`)
-    await waitForTitle(browser, 'checkinTrainingCenterBookable.ez')
+    await waitForTitle('checkinTrainingCenterBookable.ez')
 
     const elements = await browser.$$('a.btn.btn_primary.btn_rgl')
     const allScripts = []
@@ -72,12 +80,12 @@ const search = async (browser) => {
     for (const idx in scripts) {
         const script = scripts[idx]
 
-        for (const idx2 in config.preferredRooms) {
-            const preferredRoom = config.preferredRooms[idx2]
+        for (const idx2 in config.preferredAccommodations) {
+            const preferredRoom = config.preferredAccommodations[idx2]
 
             if (0 < script.indexOf(preferredRoom)) {
                 try {
-                    await reserve(browser, script)
+                    await reserve(script)
                 } catch(e) {
                     console.error(e)
                 }
@@ -86,9 +94,21 @@ const search = async (browser) => {
     }
 }
 
-const reserve = async (browser, script) => {
+const reserve = async (script) => {
+    console.log('reserve')
     await browser.execute(script)
-    await waitForTitle(browser, 'checkinTrainingCenterStepTwo.ez')
+    await waitForTitle('checkinTrainingCenterStepTwo.ez')
+
+    const rooms = await getRooms()
+    const selectedRoom = rooms.filter((r) => {
+        return -1 < r.name.indexOf(config.roomName)
+    })
+
+    if (selectedRoom[0].count === 0) {
+        console.log('reserve failed: no room')
+        await goToBookablePage()
+        return
+    }
 
     await browser.$(`#cal${config.date}`).click()
     const roomElements = await browser.$$('select#roomCd>option')
@@ -111,11 +131,36 @@ const reserve = async (browser, script) => {
     const isAlertOpen = await browser.isAlertOpen()
 
     if(isAlertOpen) {
+        console.log('reserve failed: alert open')
         await browser.dismissAlert()
-        await goToBookablePage(browser)
+        await goToBookablePage()
         return
     }
 
     await browser.$$('select#useManCnt>option')[2].click()
     await browser.$$('select#useChildCnt>option')[2].click()
+}
+
+
+const getRooms = async () => {
+    const table = await browser.$('table.tbl_calendar');
+    await table.waitForExist({ timeout: 5000 });
+
+    const roomElements = await browser.$$('#cal20240116 div.box_preview_room')
+    const rooms = []
+
+    for (const idx in roomElements) {
+        const roomElement = roomElements[idx]
+
+        if(!roomElement.getText) {
+            continue
+        }
+
+        const name = (await roomElement.getText()).split(':')[0].trim()
+        const count = await browser.$(roomElement).$('span').getText()
+
+        rooms.push({"name": name, "count": parseInt(count)})
+    }
+
+    return rooms
 }
