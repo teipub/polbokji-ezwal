9082a67dca925036cb90c4c7812f5119c9db0a4e
tei <tei@ai-dapps.com>
2023-12-21 09:46:44 +0900

    dkr

--- index.js
+++ index.js
@@ -25,43 +25,12 @@ const makeBrowser = async () => {
     await browser.$('.lgn_ip_lgn').click()
     await waitForTitle(browser, 'cuser/main.ez')
 
-    // 예약 페이지로 이동
-    await browser.url(site.reservation)
-    await waitForTitle(browser, 'checkinTrainingCenterBookable.ez')
+    await goToBookablePage(browser)
 
     while(true) {
         await search(browser)
         await browser.pause(config.waitMilliseconds)
     }
-
-    //
-    //
-    // for (const idx in scripts) {
-    //     console.log(scripts[idx])
-    // }
-    //
-    // if (0 < scripts.length) {
-    //     await browser.execute(script[0])
-    //     await waitForTitle(browser, 'checkinTrainingCenterStepTwo.ez')
-    //
-    //     await browser.$(`#cal${config.date}`).click()
-    //     const roomElements = await browser.$$('select#roomCd>option')
-    //
-    //     for (const idx in roomElements) {
-    //         const roomElement = roomElements[idx]
-    //         const roomText = await roomElement.getText()
-    //
-    //         if (0 < roomText.indexOf(config.roomName)) {
-    //             await roomElement.click()
-    //             break
-    //         }
-    //     }
-    //
-    //     await browser.$$('select#dayDiff>option')[1].click()
-    //     await browser.$$('select#useManCnt>option')[2].click()
-    //     await browser.$$('select#useChildCnt>option')[2].click()
-    // }
-    //
 })().catch((e) => console.error(e))
 
 const waitForTitle = async (browser, title) => {
@@ -71,6 +40,13 @@ const waitForTitle = async (browser, title) => {
         timeout: 10000,
         timeoutMsg: 'expected text to be different after 10s'
     })
+}
+
+
+// 예약 페이지로 이동
+const goToBookablePage = async (browser) => {
+    await browser.url(site.reservation)
+    await waitForTitle(browser, 'checkinTrainingCenterBookable.ez')
 }
 
 
@@ -129,6 +105,17 @@ const reserve = async (browser, script) => {
 
     await browser.$('select#dayDiff').waitForEnabled()
     await browser.$$('select#dayDiff>option')[1].click()
+
+    // 인원이 없을때
+    await browser.pause(1000)
+    const isAlertOpen = await browser.isAlertOpen()
+
+    if(isAlertOpen) {
+        await browser.dismissAlert()
+        await goToBookablePage(browser)
+        return
+    }
+
     await browser.$$('select#useManCnt>option')[2].click()
     await browser.$$('select#useChildCnt>option')[2].click()
 }
